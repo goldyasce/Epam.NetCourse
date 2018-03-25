d6cc1bfe10e2c9306bd72613be15817c792f0f12
goldyasce <trubin.v.a2015@gmail.com>
2018-03-25 22:50:02 +0300

    Task7 Signed-off-by: goldyasce <trubin.v.a2015@gmail.com>

---
+++ Trubin_Vladimir_Task7/Task1/Enumerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Enumerator
+    {
+        public bool MoveNext()
+        {
+            return false;
+        }
+
+        public object Current
+        {
+            get { return null; }
+        }
+    }
+}
